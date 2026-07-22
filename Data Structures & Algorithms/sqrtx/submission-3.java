be4c798ee0e1430219791df266b868b38f59b477
subhashsk1441 <subhashsk1441@gmail.com>
2026-07-22 08:42:24 +0530

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int mySqrt(int x) {
+        int l = 0 , r = x , res = x ;
+
+        while(l<=r){
+            int mid = l+((r-l)/2);
+
+            if((long) mid*mid >x){
+                r = mid -1;
+            }else if ((long)mid *mid <x){
+                l = mid +1;
+                res = mid;
+            }else{
+                return mid;
+            }
+        }
+
+        return res ;
+    }
+}
