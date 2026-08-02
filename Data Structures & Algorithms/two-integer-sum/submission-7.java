088a91fdd85606576d0ad1ffea0ebfab39f7028d
subhashsk1441 <subhashsk1441@gmail.com>
2026-08-02 09:13:49 +0530

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> num = new HashMap<>();
+
+        for(int i = 0 ; i<= nums.length-1;i++){
+
+            int diff = target - nums[i];
+
+            if(num.containsKey(diff)){
+                return new int []{ num.get(diff), i};
+            }
+
+            num.put(nums[i],i);
+        }
+
+        return new int []{};
+    }
+}
