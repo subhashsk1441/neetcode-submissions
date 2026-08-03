7762bc4b9bbe1a8bf2cfc876accd550d0f2dc504
subhashsk1441 <subhashsk1441@gmail.com>
2026-08-03 08:29:07 +0530

    Add: duplicate-integer - submission-20

---
+++ Data Structures & Algorithms/duplicate-integer/submission-20.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        
+        HashSet<Integer> unique = new HashSet<>();
+
+        for(int n : nums){
+            if(unique.contains(n)){
+                return true ;
+            }
+
+            unique.add(n);
+        }
+
+
+        return false;
+    }
+}
