f5dcb7de9e3d3bec18576d307978de1d58f96b10
subhashsk1441 <subhashsk1441@gmail.com>
2026-08-04 22:32:54 +0530

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+         String prefix = strs[0];
+
+         for(int i = 1 ; i < strs.length; i++){
+            int j = 0 ;
+
+            int k = Math.min(prefix.length(), strs[i].length());
+
+            while(j<k){
+                if(prefix.charAt(j) != strs[i].charAt(j)){
+                    break;
+                }
+
+                j++;
+            }
+            prefix = prefix.substring(0,j);
+         }
+
+         return prefix;
+    }
+}
